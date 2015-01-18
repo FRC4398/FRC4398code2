08387ef1888aac3cc37f7f7b1cbca888982a7e50
denchief1 <jacob@iharrand.com>
2015-01-17 21:44:23 -0500

    changes to the Robot code

--- try 2/src/Robot.cpp
+++ try 2/src/Robot.cpp
@@ -3,19 +3,17 @@
 class Robot: public IterativeRobot
 {
 
-	RobotDrive myRobot; // robot drive system
-	Joystick *stick; // only joystick
-	Talon *rightDrive;
-	Talon *leftDrive;
+	RobotDrive myRobot = new Talon(0, 1); // robot drive system
+	Joystick stick = new Joystick(1); // only joystick
+	Talon Elevator = new Talon(2);
 	LiveWindow *lw;
 	int autoLoopCounter;
 
 public:
-	RobotDemo(void) :
-		myRobot(1, 2),	// these must be initialized in the same order
-		stick = new Joystick(1);		// as they are declared above.
-		rightDrive = new Talon(1);
-		leftDrive = new Talon(2)
+	Robot() :
+		myRobot(0, 1), // must be intialized in same order
+		stick(1),
+		Elevator(2),
 		lw(NULL),
 		autoLoopCounter(0)
 	{
@@ -35,7 +33,7 @@ private:
 
 	void AutonomousPeriodic()
 	{
-		if(autoLoopCounter < 100) //Check if we've completed 100 loops (approximately 2 seconds)
+		if(autoLoopCounter < 2) //Check if we've completed 100 loops (approximately 2 seconds)
 		{
 			myRobot.Drive(-0.5, 0.0); 	// drive forwards half speed
 			autoLoopCounter++;
@@ -51,9 +49,15 @@ private:
 
 	void TeleopPeriodic(void)
 	{
-		myRobot.ArcadeDrive(stick->GetRawAxis(Yaxis); // drive with arcade style (use right stick)
-		myRobot.ArcadeDrive(stick->GetRawAxis(Xaxis);
-		myRobot.ArcadeDrive(SetSpeed(Y);
+		myRobot.ArcadeDrive(stick); // drive with arcade style (use right stick)
+		if (stick.GetTrigger == 1) //raise the elevator
+		{
+				Elevator.Set(-0.7);
+		}
+		else if (stick.GetTrigger == 0)
+		{
+				Elevator.Set(0.0);
+		}
 	}
 
 	void TestPeriodic()
